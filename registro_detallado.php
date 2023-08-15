0740514ee4f4e742c1e5fa6b5d61c3f8ee64a9e1
William Morales <114114124+UserWm@users.noreply.github.com>
2023-08-14 21:51:46 -0600

    Create registro_detallado.php

---
+++ registro_detallado.php
@@ -0,0 +1,15 @@
+<?php
+$nombre= $_POST['nombre'];
+$apellido=$_POST['apellido'];
+$dui=$_POST['dui'];
+$telefono=$_POST['telefono'];
+$correo=$_POST['correo'];
+
+echo "Nombre:".$nombre."<br>";
+echo "Apellido:".$apellido."<br>";
+echo "DUI:".$dui."<br>";
+echo "Telefono:".$telefono."<br>";
+echo "Correo:".$correo."<br>";
+
+?>
+
